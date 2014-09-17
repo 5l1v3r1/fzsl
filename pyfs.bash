6ddf75ca78e096afa93bc1e6d2592a79b68b63ce
Justin Bronder <jsbronder@gmail.com>
2014-09-17 14:55:01 -0400

    sh:  add initial mockup of shell expansion

---
+++ pyfs.bash
@@ -0,0 +1,11 @@
+
+__pyfs_expand() {
+    # TODO:  obviously this needs a real path
+    python ~/scm/pyfs/pyfs/ui.py
+}
+
+bind '"\C-x\C-E": shell-expand-line'
+bind '"\C-x\C-R": redraw-current-line'
+bind '"\C-t": "\e$a \eddi$(__pyfs_expand)\C-x\C-E\e0Px$a \C-x\C-R\exa"'
+bind -m vi-command '"\C-t": "i\C-t"'
+
