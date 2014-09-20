2e06e17aac963cd6c72475a1836f0811f4999621
Justin Bronder <jsbronder@gmail.com>
2014-09-20 00:52:25 -0400

    fzsl:  add emacs bindings

--- fzsl.bash
+++ fzsl.bash
@@ -1,5 +1,10 @@
-bind '"\C-x\C-E": shell-expand-line'
-bind '"\C-x\C-R": redraw-current-line'
-bind '"\C-t": "\e$a \eddi$(fzsl)\C-x\C-E\e0Pa \exddi\n\epa \C-x\C-R"'
-bind -m vi-command '"\C-t": "i\C-t"'
+if set -o | grep -q 'vi\s*on'; then
+    bind '"\C-x\C-E": shell-expand-line'
+    bind '"\C-x\C-R": redraw-current-line'
+    bind '"\C-t": "\e$a \eddi$(fzsl)\C-x\C-E\e0Pa \exddi\n\epa \C-x\C-R"'
+    bind -m vi-command '"\C-t": "i\C-t"'
+else
+    bind '"\eR": redraw-current-line'
+    bind '"\C-t": " \C-u \C-a\C-k$(fzsl)\e\C-e\C-y\C-a\C-y\ey\C-a\C-k\n\C-y\C-e\eR \C-h"'
+fi
 
