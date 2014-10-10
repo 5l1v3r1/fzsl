533f951f5589fa65e8fb38f5219beb31c9a11b6e
Justin Bronder <jsbronder@gmail.com>
2014-10-09 22:30:56 -0400

    setup:  updating in preperation for pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*paths):
 
 setuptools.setup(
         name='fzsl',
-        version='1.0',
+        version='0.1',
         description='Fuzzy path searching for shells',
         license='BSD',
         long_description=(read('README.txt')),
@@ -18,15 +18,17 @@ setuptools.setup(
         author_email='jsbronder@gmail.com',
         url='http://github.com/jsbronder/fzsl',
         include_package_data=True,
-        keywords='fuzzy shell search',
+        keywords='fuzzy shell search console match ncurses',
         packages=['fzsl'],
         data_files=[
             ('%s/etc/fzsl' % (sys.prefix,), ['etc/fzsl.bash', 'etc/fzsl.conf']),
         ],
         scripts=['bin/fzsl'],
+        install_requires=['six'],
         classifiers=[
             'Development Status :: 3 - Alpha',
             'Environment :: Console',
+            'Environment :: Console :: Curses',
             'Intended Audience :: Developers',
             'Intended Audience :: End Users/Desktop',
             'Intended Audience :: System Administrators',
@@ -38,5 +40,14 @@ setuptools.setup(
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
-            'Topic :: Software Development :: Libraries :: Python Modules',],
+            'Topic :: System :: Shells',
+            'Topic :: System :: Systems Administration',
+            'Topic :: System :: System Shells',
+            'Topic :: Terminals',
+            'Topic :: Terminals :: Serial',
+            'Topic :: Terminals :: Telnet',
+            'Topic :: Terminals :: Terminal Emulators/X Terminals',
+            'Topic :: Text Processing :: Filters',
+            'Topic :: Utilities',
+        ]
 )
