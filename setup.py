e029a51f3d69413255c73007e2f53d7ee7176f6a
Justin Bronder <jsbronder@gmail.com>
2014-10-09 21:30:30 -0400

    setup:  update language classifiers

--- setup.py
+++ setup.py
@@ -35,5 +35,8 @@ setuptools.setup(
             'Programming Language :: Python',
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
             'Topic :: Software Development :: Libraries :: Python Modules',],
 )
