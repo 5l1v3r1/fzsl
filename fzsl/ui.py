5l1v3r1/fzsl

commit c7d81ac7fd4c3093ed56a7d8d054a49f0248a2f3

fzsl:  pacify flake

fzsl/ui.py
@@ -7,9 +7,8 @@ import six
 
 import fzsl
 
-for i, color in enumerate(('white', 'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan')):
-    vars()['COL_%s' % (color.upper())] = i
-    vars()['COL_B%s' % (color.upper())] = i + 8
+
+COL_BCYAN = 15
 
 
 @contextlib.contextmanager
@@ -37,13 +36,14 @@ def ncurses():
     # Reduce the timeout after receiving an escape character, there
     # doesn't seem to be a way to do this via curses so we have to
     # set the environment variable before creating the screen.
-    if not 'ESCDELAY' in os.environ:
-        os.environ['ESCDELAY'] =  '25'
+    if 'ESCDELAY' not in os.environ:
+        os.environ['ESCDELAY'] = '25'
 
     scr = curses.initscr()
     curses.start_color()
     curses.use_default_colors()
-    _ = [curses.init_pair(i + 1, i, -1) for i in range(curses.COLORS)]
+    for i in range(curses.COLORS):
+        curses.init_pair(i + 1, i, -1)
 
     curses.noecho()
     curses.cbreak()
@@ -55,7 +55,6 @@ def ncurses():
     exc = None
     try:
         yield scr
-    #pylint: disable=W0703
     except Exception:
         exc = sys.exc_info()
 
@@ -93,7 +92,7 @@ class SimplePager(object):
 
         y, x = self._scr.getmaxyx()
 
-        self._prompt = curses.newwin(1, x, y-1, 0)
+        self._prompt = curses.newwin(1, x, y - 1, 0)
         self._select = curses.newwin(y - 2, x, 0, 0)
         self._max_y = y - 2
         self._max_x = x - 1
@@ -133,9 +132,13 @@ class SimplePager(object):
 
             self._select.addstr(line, 0, prefix + match[:start], decor)
             if start + offset < self._max_x:
-                self._select.addstr(line, start+offset, match[start:end], decor|curses.color_pair(COL_BCYAN))
+                self._select.addstr(
+                    line,
+                    start + offset,
+                    match[start:end],
+                    decor | curses.color_pair(COL_BCYAN))
             if end + offset < self._max_x:
-                self._select.addstr(line, end+offset, match[end:], decor)
+                self._select.addstr(line, end + offset, match[end:], decor)
         self._select.refresh()
 
     def _draw_prompt(self):
@@ -171,13 +174,15 @@ class SimplePager(object):
             if key in (u'^M',):
                 # enter
                 break
-            elif key  in ('KEY_DOWN', '^J'):
+            elif key in ('KEY_DOWN', '^J'):
                 # down arrow, ctrl+j
-                self._selection = self._selection - 1 if self._selection > 0 else 0
+                if self.selection > 0:
+                    self._selection -= 1
                 self._draw_select()
-            elif key  in ('KEY_UP', '^K'):
+            elif key in ('KEY_UP', '^K'):
                 # up arrow, ctrl+k
-                self._selection = self._selection + 1 if self._selection < self._max_y - 2 else self._selection
+                if self._selection < self._max_y - 2:
+                    self._selection += 1
                 self._draw_select()
             elif key in ('KEY_LEFT',):
                 if self._cursor_x > 0:
@@ -197,8 +202,8 @@ class SimplePager(object):
             elif c in (curses.KEY_RESIZE,):
                 y, x = self._scr.getmaxyx()
 
-                # Have to handle the parent screen fully first otherwise updates
-                # to the subwindows don't seem to take.
+                # Have to handle the parent screen fully first otherwise
+                # updates to the subwindows don't seem to take.
                 curses.resizeterm(y, x)
                 self._scr.resize(y, x)
                 self._scr.erase()
@@ -212,7 +217,7 @@ class SimplePager(object):
                 self._draw_select()
 
                 self._prompt.resize(1, x)
-                self._prompt.mvwin(y-1, 0)
+                self._prompt.mvwin(y - 1, 0)
                 self._draw_prompt()
             else:
                 if key in ('KEY_BACKSPACE',):
@@ -244,4 +249,3 @@ class SimplePager(object):
             return self._scanner.transform(match)
         except IndexError:
             return ''
-

bin/fzsl
@@ -1,6 +1,5 @@
 #!/usr/bin/env python2
 
-import fcntl
 import getopt
 import os
 import sys
@@ -34,6 +33,7 @@ CONFIGURATION FILE:
     %s will be used.
 """ % (__default_config_path__,)
 
+
 def read_config(path=None):
     parser = configparser.RawConfigParser(allow_no_value=True)
 
@@ -45,6 +45,7 @@ def read_config(path=None):
     parser.read(path)
     return parser
 
+
 def build_scanners(parser):
     scanners = []
     for section in parser.sections():
@@ -55,6 +56,7 @@ def build_scanners(parser):
     scanners.sort()
     scanners.reverse()
     return scanners
+
 
 def pick_scanner(scanners):
     cwd = os.getcwd()
@@ -109,11 +111,11 @@ def main():
             print('Rule "%s" is not defined' % (rule,))
             sys.exit(1)
 
-
     with fzsl.ncurses() as scr:
         ui = fzsl.SimplePager(scr, scanner)
         result = ui.run()
     sys.stdout.write(result.strip())
+
 
 if __name__ == '__main__':
     main()

fzsl/__init__.py
@@ -18,3 +18,18 @@ from . scanner import (
     UnknownTypeError,
 )
 
+__all__ = [
+    'FuzzyMatch',
+
+    'ncurses',
+    'SimplePager',
+
+    'ConfigError',
+    'NoTypeError',
+    'Scanner',
+    'scanner_from_configparser',
+    'SimpleScanner',
+    'StaticScanner',
+    'SubprocessError',
+    'UnknownTypeError',
+]

fzsl/core.py
@@ -4,6 +4,7 @@ import multiprocessing
 import re
 import signal
 
+
 class MatchInfo(object):
     def __init__(self, start=0, end=0, score=0, round_ejected=0):
         """
@@ -12,8 +13,8 @@ class MatchInfo(object):
         @attr start         - index in path where match starts
         @attr end           - index in path where match ends
         @attr score         - match score
-        @attr round_ejected - the round (length of search string) that this path
-                              was removed as a possible match
+        @attr round_ejected - the round (length of search string) that this
+                              path was removed as a possible match
         """
         self.start = start
         self.end = end
@@ -24,10 +25,11 @@ class MatchInfo(object):
         """
         Update the MatchInfo attributes
 
-        @param start         - index in path where match starts
-        @param end           - index in path where match ends
-        @param score         - match score
-        @param round_ejected - the round (length of search string) that this path
+        @param start            - index in path where match starts
+        @param end              - index in path where match ends
+        @param score            - match score
+        @param round_ejected    - the round (length of search string) that this
+                                  path
         """
         if start is not None:
             self.start = start
@@ -37,6 +39,7 @@ class MatchInfo(object):
             self.score = score
         if round_ejected is not None:
             self.round_ejected = round_ejected
+
 
 def default_scorer(path, c_round, regex):
     """
@@ -72,6 +75,7 @@ def default_scorer(path, c_round, regex):
     else:
         return path, (0, 0, 0.0, c_round)
 
+
 class FuzzyMatch(object):
     def __init__(self, files=None, scorer=default_scorer):
         """
@@ -85,7 +89,7 @@ class FuzzyMatch(object):
                           default_scorer.  See it for more information.
         """
         if files is not None:
-            self._library = {path:MatchInfo() for path in files}
+            self._library = {path: MatchInfo() for path in files}
         else:
             self._library = {}
 
@@ -102,7 +106,8 @@ class FuzzyMatch(object):
         """
         Number of paths which are candidates given the current query
         """
-        return len([info for info in self._library.values() if info.round_ejected == 0])
+        return len([info for info in self._library.values()
+            if info.round_ejected == 0])
 
     @property
     def n_files(self):
@@ -118,7 +123,7 @@ class FuzzyMatch(object):
 
         @param files    - list of files to add.
         """
-        self._library.update({path:MatchInfo() for path in files})
+        self._library.update({path: MatchInfo() for path in files})
 
     def reset_files(self, files):
         """
@@ -128,7 +133,7 @@ class FuzzyMatch(object):
 
         @param files    - new files to use as a library
         """
-        self._library = {path:MatchInfo() for path in files}
+        self._library = {path: MatchInfo() for path in files}
 
     def update_scores(self, search):
         """
@@ -142,9 +147,9 @@ class FuzzyMatch(object):
         s_len = len(search)
 
         if s_len < len(self._search):
-            _ = [self._library[path].update(round_ejected=0)
-                    for path, info in self._library.items()
-                    if info.round_ejected == s_len + 1]
+            for path, info in self._library.items():
+                if info.round_ejected == s_len + 1:
+                    self._library[path].update(round_ejected=0)
 
         self._search = search
 
@@ -154,13 +159,6 @@ class FuzzyMatch(object):
             # In the single character search case, it's far faster to use the
             # string index function over regex.  This is important as we want
             # to slim down the potential matches as quickly as possible.
-            #
-            # > def f(): 'aoacoeaemaoceaimfoaijhaohfeiahfoefhoeia'.index(j)
-            # > def f2(): re.search('j', 'aoacoeaemaoceaimfoaijhaohfeiahfoefhoeia')
-            # > timeit.timeit('f()', 'from __main__ import f')
-            # 0.5705671310424805
-            # > timeit.timeit('f()', 'from __main__ import f2 as f')
-            # 2.7029879093170166
             def quick_score(path, info):
                 index = path.find(search)
                 if index != -1:
@@ -168,19 +166,19 @@ class FuzzyMatch(object):
                 else:
                     info.update(round_ejected=1)
 
-            _ = [quick_score(path, info) for path, info in self._library.items()]
+            for path, info in self._library.items():
+                quick_score(path, info)
             return
 
         pattern = '(?=(' + '.*?'.join(re.escape(c) for c in search) + '))'
         regex = re.compile(pattern, re.IGNORECASE)
 
         scorer = functools.partial(self._scorer, c_round=s_len, regex=regex)
-        candidates = [path for path, info
-                in self._library.items()
-                if info.round_ejected == 0]
+        candidates = [path for path, info in self._library.items()
+            if info.round_ejected == 0]
 
-        _ = [self._library[path].update(*update)
-                for path, update in self._pool.map(scorer, candidates)]
+        for path, update in self._pool.map(scorer, candidates):
+            self._library[path].update(*update)
 
     def score(self, path):
         """
@@ -192,21 +190,23 @@ class FuzzyMatch(object):
     def start(self, path):
         """
         @param path - path to lookup
-        @return     - index of the start of the match of the current query in the path
+        @return     - index of the start of the match of the current query in
+                      the path
         """
         return self._library[path].start
 
     def end(self, path):
         """
         @param path - path to lookup
-        @return     - index of the end of the match of the current query in the path
+        @return     - index of the end of the match of the current query in the
+                      path
         """
         return self._library[path].end
 
     def top_matches(self, depth=10):
         """
-        Get the best matching paths in the library.  Note that only paths
-        which have not been ejected and have a positive score will be returned.
+        Get the best matching paths in the library.  Note that only paths which
+        have not been ejected and have a positive score will be returned.
         Therefore, the length of the returned list may be less than the
         specified depth.
 
@@ -220,6 +220,8 @@ class FuzzyMatch(object):
         else:
             valid = self._library.keys()
 
-        ret = heapq.nlargest(depth, valid, key=lambda x: self._library[x].score)
+        ret = heapq.nlargest(
+            depth,
+            valid,
+            key=lambda x: self._library[x].score)
         return ret
-

fzsl/scanner.py
@@ -11,14 +11,18 @@ class SubprocessError(Exception):
         super(SubprocessError, self).__init__(
                 'Failed to run: "%s" in %s: %s' % (cmd, cwd, error))
 
+
 class NoTypeError(Exception):
     pass
+
 
 class UnknownTypeError(Exception):
     pass
 
+
 class ConfigError(Exception):
     pass
+
 
 @functools.total_ordering
 class Scanner(object, six.with_metaclass(abc.ABCMeta)):
@@ -33,7 +37,6 @@ class Scanner(object, six.with_metaclass(abc.ABCMeta)):
         """
         self._name = name
         self._priority = priority
-
 
     def __eq__(self, other):
         return (self._name == other._name
@@ -83,7 +86,14 @@ class Scanner(object, six.with_metaclass(abc.ABCMeta)):
 
 
 class SimpleScanner(Scanner):
-    def __init__(self, name, cmd, priority=0, detect_cmd=None, root_path=None, cache=None):
+    def __init__(
+            self,
+            name,
+            cmd,
+            priority=0,
+            detect_cmd=None,
+            root_path=None,
+            cache=None):
         """
         Create a scanner.
 
@@ -127,10 +137,16 @@ class SimpleScanner(Scanner):
                             stderr=subprocess.PIPE)
                     stdout, stderr = c.communicate()
                 except OSError:
-                    raise SubprocessError(root_path[1:], os.path.realpath(os.curdir), stderr)
+                    raise SubprocessError(
+                        root_path[1:],
+                        os.path.realpath(os.curdir),
+                        stderr)
 
                 if c.returncode != 0:
-                    raise SubprocessError(root_path[1:], os.path.realpath(os.curdir), stderr)
+                    raise SubprocessError(
+                        root_path[1:],
+                        os.path.realpath(os.curdir),
+                        stderr)
 
                 self._root_path = stdout.strip().decode('UTF-8')
             else:
@@ -158,7 +174,8 @@ class SimpleScanner(Scanner):
         """
         kwds = {}
         if parser.has_option(section, 'detect_cmd'):
-            kwds['detect_cmd'] = parser.get(section, 'detect_cmd').replace('\n', ' ')
+            dcmd = parser.get(section, 'detect_cmd').replace('\n', ' ')
+            kwds['detect_cmd'] = dcmd
 
         if parser.has_option(section, 'root_path'):
             kwds['root_path'] = parser.get(section, 'root_path')
@@ -369,6 +386,7 @@ def plugin_scanner_from_configparser(section, parser):
 
     return scanner
 
+
 def scanner_from_configparser(section, parser):
     """
     Create a Scanner from a config parser section.
@@ -392,4 +410,3 @@ def scanner_from_configparser(section, parser):
             scanner_type, section))
 
     return scanner
-

commit ce060e3c6674b3dfa07be3b4394112e423e8629e

ui: fix moving selection down
Fix bug introduced in c7d81ac7.

fzsl/ui.py
@@ -174,7 +174,7 @@ class SimplePager(object):
                 break
             elif key in ('KEY_DOWN', '^J'):
                 # down arrow, ctrl+j
-                if self.selection > 0:
+                if self._selection > 0:
                     self._selection -= 1
                 self._draw_select()
             elif key in ('KEY_UP', '^K'):