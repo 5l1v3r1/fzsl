8281efce49cb62b4b4884ef316d8aae981c85aad
Justin Bronder <jsbronder@cold-front.org>
2019-11-08 17:48:06 -0500

    test: switch to pytest

--- test/test_core.py
+++ test/test_core.py
@@ -1,101 +1,101 @@
-import os
-import sys
-import unittest
-
-TESTDIR = os.path.realpath(os.path.dirname(__file__))
-sys.path.insert(0, os.path.realpath(os.path.join(TESTDIR, "..")))
+import pytest
 
 import fzsl
 
 
-class SimpleScannerTest(unittest.TestCase):
-    def setUp(self):
-        self.fm = fzsl.FuzzyMatch()
+def test_library():
+    fm = fzsl.FuzzyMatch()
 
-    def test_library(self):
-        self.assertEqual(0, self.fm.n_matches)
-        self.assertEqual(0, self.fm.n_files)
-        self.assertEqual([], self.fm.top_matches())
+    assert 0 == fm.n_matches
+    assert 0 == fm.n_files
+    assert [] == fm.top_matches()
 
-        with self.assertRaises(KeyError):
-            self.fm.score("a")
+    with pytest.raises(KeyError):
+        fm.score("a")
 
-        self.fm.add_files(["a", "b", "c"])
-        self.assertEqual(3, self.fm.n_matches)
-        self.assertEqual(3, self.fm.n_files)
-        self.assertEqual(["a", "b", "c"], sorted(self.fm.top_matches()))
+    fm.add_files(["a", "b", "c"])
+    assert 3 == fm.n_matches
+    assert 3 == fm.n_files
+    assert ["a", "b", "c"] == sorted(fm.top_matches())
 
-        self.assertEqual(0, self.fm.score("a"))
-        self.assertEqual(0, self.fm.start("a"))
-        self.assertEqual(0, self.fm.end("a"))
+    assert 0 == fm.score("a")
+    assert 0 == fm.start("a")
+    assert 0 == fm.end("a")
 
-        self.fm.reset_files([])
-        self.assertEqual(0, self.fm.n_matches)
-        self.assertEqual(0, self.fm.n_files)
-        self.assertEqual([], self.fm.top_matches())
-
-    def test_update_scores(self):
-        files = ["abc/def/ghi", "abc/d", "abc/ggg", "z/1", "z/2", "zz/3"]
-
-        self.fm.add_files(files)
-
-        self.fm.update_scores("abc")
-        self.assertEqual(3, self.fm.n_matches)
-        matches = self.fm.top_matches()
-
-        self.fm.update_scores("abcd")
-        self.assertEqual(2, self.fm.n_matches)
-
-        self.fm.update_scores("abc")
-        self.assertEqual(3, self.fm.n_matches)
-        self.assertEqual(matches, self.fm.top_matches())
-
-        self.fm.update_scores("ab")
-        self.fm.update_scores("a")
-        self.fm.update_scores("")
-        self.assertEqual(self.fm.n_matches, self.fm.n_files)
-
-    def test_prefer_latter(self):
-        files = ["prefix/abc/stuff", "abc/d", "some/prefix/abc"]
-
-        self.fm.add_files(files)
-        self.fm.update_scores("abc")
-        self.assertEqual("some/prefix/abc", self.fm.top_matches(1)[0])
-
-    def test_prefer_shorter(self):
-        files = ["a/z/b/z/c", "a/b/c", "a/bbbbb/c"]
-
-        self.fm.add_files(files)
-        self.fm.update_scores("abc")
-        self.assertEqual("a/b/c", self.fm.top_matches(1)[0])
-
-    def test_start_end(self):
-        files = ["abc/def/ghi", "ggg/a/b/ggg/c/d", "ggg/abc"]
-
-        self.fm.add_files(files)
-        self.fm.update_scores("abc")
-
-        self.assertEqual(0, self.fm.start("abc/def/ghi"))
-        self.assertEqual(2, self.fm.end("abc/def/ghi"))
-
-        self.assertEqual(4, self.fm.start("ggg/a/b/ggg/c/d"))
-        self.assertEqual(12, self.fm.end("ggg/a/b/ggg/c/d"))
-
-        self.assertEqual(4, self.fm.start("ggg/abc"))
-        self.assertEqual(6, self.fm.end("ggg/abc"))
-
-    def test_ignorecase(self):
-        files = ["abc/def/ghi", "abc/d", "abc/ggg", "z/1", "z/2", "zz/3"]
-
-        self.fm.add_files(files)
-
-        self.fm.update_scores("ABC")
-        self.assertEqual(3, self.fm.n_matches)
+    fm.reset_files([])
+    assert 0 == fm.n_matches
+    assert 0 == fm.n_files
+    assert [] == fm.top_matches()
 
 
-def main():
-    unittest.main()
+def test_update_scores():
+    fm = fzsl.FuzzyMatch()
+
+    files = ["abc/def/ghi", "abc/d", "abc/ggg", "z/1", "z/2", "zz/3"]
+
+    fm.add_files(files)
+
+    fm.update_scores("abc")
+    assert 3 == fm.n_matches
+    matches = fm.top_matches()
+
+    fm.update_scores("abcd")
+    assert 2 == fm.n_matches
+
+    fm.update_scores("abc")
+    assert 3 == fm.n_matches
+    assert matches == fm.top_matches()
+
+    fm.update_scores("ab")
+    fm.update_scores("a")
+    fm.update_scores("")
+    assert fm.n_matches == fm.n_files
 
 
-if __name__ == "__main__":
-    main()
+def test_prefer_latter():
+    fm = fzsl.FuzzyMatch()
+
+    files = ["prefix/abc/stuff", "abc/d", "some/prefix/abc"]
+
+    fm.add_files(files)
+    fm.update_scores("abc")
+    assert "some/prefix/abc" == fm.top_matches(1)[0]
+
+
+def test_prefer_shorter():
+    fm = fzsl.FuzzyMatch()
+
+    files = ["a/z/b/z/c", "a/b/c", "a/bbbbb/c"]
+
+    fm.add_files(files)
+    fm.update_scores("abc")
+    assert "a/b/c" == fm.top_matches(1)[0]
+
+
+def test_start_end():
+    fm = fzsl.FuzzyMatch()
+
+    files = ["abc/def/ghi", "ggg/a/b/ggg/c/d", "ggg/abc"]
+
+    fm.add_files(files)
+    fm.update_scores("abc")
+
+    assert 0 == fm.start("abc/def/ghi")
+    assert 2 == fm.end("abc/def/ghi")
+
+    assert 4 == fm.start("ggg/a/b/ggg/c/d")
+    assert 12 == fm.end("ggg/a/b/ggg/c/d")
+
+    assert 4 == fm.start("ggg/abc")
+    assert 6 == fm.end("ggg/abc")
+
+
+def test_ignorecase():
+    fm = fzsl.FuzzyMatch()
+
+    files = ["abc/def/ghi", "abc/d", "abc/ggg", "z/1", "z/2", "zz/3"]
+
+    fm.add_files(files)
+
+    fm.update_scores("ABC")
+    assert 3 == fm.n_matches
